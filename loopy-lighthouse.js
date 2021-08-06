f3899b66ce6f54e01e0d8d020596df5608720e1b
habib.codes <habib.codes@gmail.com>
2021-08-06 13:19:07 -0400

    scratch sheet for fizzBizz/loopyLighthouse exercise

---
+++ loopy-lighthouse.js
@@ -0,0 +1,13 @@
+// loopy lighthouse conditional loop
+
+for (let i = 100; i <= 200; i++) {
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log('LoopyLighthouse');
+  } else if (i % 4 === 0) {
+    console.log('Lighthouse');
+  } else if (i % 3 === 0) {
+    console.log('Loopy');
+  } else {
+    console.log(i);
+  }
+}
