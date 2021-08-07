6ef54e469029b0ef943e3eabd65fd63e0ddf993d
habib.codes <habib.codes@gmail.com>
2021-08-07 13:24:31 -0400

    declaring and calling functions

---
+++ hello.js
@@ -0,0 +1,17 @@
+// functions //
+
+//sayHello() function declaration
+const sayHello = function () {
+  console.log('Hello world');
+};
+
+sayHello();
+
+//sayHello(name) function declaration with parameter
+const sayHello2 = function (name) {
+  console.log('Hello ' + name);
+};
+
+sayHello2('Frodo');
+sayHello2('Samwise');
+sayHello2('Pippin');
