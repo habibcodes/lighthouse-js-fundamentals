19fb8f604c7f852c6ce8029173a389cdd7e472bb
habib.codes <habib.codes@gmail.com>
2021-08-09 12:31:26 -0400

    for-loop added to file

---
+++ packing-list.js
@@ -0,0 +1,19 @@
+// Iterating Over Arrays Assignment
+
+// for-loops
+const packingList = [
+  'bowls',
+  'plates',
+  'pots',
+  'pans',
+  'eating utensils',
+  'glasses',
+  'cups',
+  'cooking utensils',
+];
+
+console.log('Kitchen stuff to pack:');
+
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
