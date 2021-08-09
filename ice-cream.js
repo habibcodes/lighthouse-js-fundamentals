2d453c84a3dc334d0bd908fc68509fa3c38eca81
habib.codes <habib.codes@gmail.com>
2021-08-09 12:00:37 -0400

    worksheet ice-cream flavours assignment

---
+++ ice-cream.js
@@ -0,0 +1,23 @@
+// Arrays - Ice-cream
+
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+console.log(iceCreamFlavours);
+
+// add 'root beer' to the array via .push();
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// log first flavour in array
+console.log(iceCreamFlavours[0]);
+
+// log the last flavour in array
+console.log(iceCreamFlavours[5]);
+
+// log total number of flavours via the .length property
+console.log(iceCreamFlavours.length);
