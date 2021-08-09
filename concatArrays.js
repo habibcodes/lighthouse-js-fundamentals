228c076d567abe7d0026c1367d433850e4998859
habib.codes <habib.codes@gmail.com>
2021-08-09 16:10:42 -0400

    more testing

--- concatArrays.js
+++ concatArrays.js
@@ -13,5 +13,10 @@ function concat(arr1, arr2) {
 // let arr = [1, 2, 3];
 // let arr2 = [4, 5, 6];
 
-console.log(concat(arr, arr2));
-console.log(arr);
+// console.log(concat(arr, arr2));
+// console.log(arr);
+
+console.log(concat([1, 2, 3], [4, 5, 6]), '=?', [1, 2, 3, 4, 5, 6]);
+console.log(concat([0, 3, 1], [9, 7, 2]), '=?', [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), '=?', [9, 7, 2]);
+console.log(concat([5, 10], []), '=?', [5, 10]);
