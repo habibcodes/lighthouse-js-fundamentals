beca2359157f5a647fa5bd1913f5084a539d4326
habib.codes <habib.codes@gmail.com>
2021-08-14 12:47:23 -0400

    used multiple lines in conditional to target and alter value of matched key; couldn't find an easier way atm

--- smartGarbage.js
+++ smartGarbage.js
@@ -12,21 +12,61 @@
 */
 
 // define bins object
-let bins = {
-  waste: 0,
-  recycling: 0,
-  compost: 0,
-};
-console.log(bins);
+// let bins = {
+//   waste: 0,
+//   recycling: 0,
+//   compost: 0,
+// };
+// console.log(bins.waste);
+// console.log(bins.recycling);
+// console.log(bins.compost);
 // define func smartGarbage(trash,bins);
-function smartGarbage(trash, bins) {
-  for (let key of Object.keys(bins)) {
-    if (trash === key) {
-      key = +1;
-      return key;
-    }
+// function smartGarbage(trash, bins) {
+//   for (let key of Object.keys(bins)) {
+//     if (trash === key) {
+//       key = +1;
+//       return key;
+//     }
+//   }
+// }
+
+const smartGarbage = (trash, bins) => {
+  if (trash === 'waste') {
+    // return (bins.waste += 1);
+    bins.waste += 1;
+    bins.recycling = bins.recycling;
+    bins.compost = bins.compost;
+    return bins;
+  } else if (trash === 'recycling') {
+    // return (bins.recycling += 1);
+    bins.waste = bins.waste;
+    bins.recycling += 1;
+    bins.compost = bins.compost;
+    return bins;
+  } else if (trash === 'compost') {
+    // return (bins.compost += 1);
+    bins.waste = bins.waste;
+    bins.recycling = bins.recycling;
+    bins.compost += 1;
+    return bins;
+  } else {
+    return 'Enter a valid number.';
   }
-}
+};
+
+// const smartGarbage = (trash, bins) => {
+//   for (const trash in bins) {
+//     if (trash === 'waste') {
+//       return (bins.waste += 1);
+//     } else if (trash === 'recycling') {
+//       return (bins.recycling += 1);
+//     } else if (trash === 'compost') {
+//       return (bins.compost += 1);
+//     } else {
+//       return 'Enter a valid number.';
+//     }
+//   }
+// };
 
 console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
 // conditional to sort "trash" type
