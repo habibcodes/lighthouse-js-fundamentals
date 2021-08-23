19d9a8df894524187d1d678786c2231f077b2091
habib.codes <habib.codes@gmail.com>
2021-08-23 17:32:14 -0400

    kata1 - sum largest two numbers in given array

---
+++ kata1_sumLargestNumbers.js
@@ -0,0 +1,22 @@
+// Kata 1 - Sum The Largest Numbers //
+
+/*
+    - create a sumLargestNumbers(); func that takes in arrays with two+ integers, identifies the largest two and sums them together
+    - the function must return the sum of the two integers
+    - you cannot use built-in JS helper funcs like sort/map/filter
+    - NOTE: SPECIAL INSTRUCTIONS FOR SUBMISSION!!
+*/
+
+const arr = [1, 7, 2, 13]; // 7+13=20
+
+const sumLargestNumbers = (data) => {
+  let x, y; 
+  for (let i = 0; i < data.length; i++){
+      if (i < )
+  }
+  //use bubble sort!
+};
+
+// console.log(sumLargestNumbers([1, 10]));
+// console.log(sumLargestNumbers([1, 2, 3]));
+// console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
