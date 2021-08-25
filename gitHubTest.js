6ea56ca3cdb6238169a0f531cc49f72f20bb1a5f
habib.codes <habib.codes@gmail.com>
2021-08-25 11:53:32 -0400

    able to push from both Vagrant and Bash CLI (ssh and pass auth have been set up)

--- gitHubTest.js
+++ gitHubTest.js
@@ -1,5 +1,7 @@
 // github push from vagrant //
 
+// setup SSH for Vagrant
 console.log('logging from Vagrant CLI!');
 
+// able to still push via Bash as well
 console.log('git push from Bash test');
